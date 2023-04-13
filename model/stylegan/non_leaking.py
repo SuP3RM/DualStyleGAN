ff0f5b0a7e2fc2b26895626c89c7582cce806ac9
Mario Aranda Jr <marand7@illinois.edu>
2023-04-12 22:49:11 -0500

    update

--- model/stylegan/non_leaking.py
+++ model/stylegan/non_leaking.py
@@ -8,6 +8,9 @@ import numpy as np
 from model.stylegan.distributed import reduce_sum
 from model.stylegan.op import upfirdn2d
 
+from pkg_resources import parse_version
+
+_use_pytorch_1_11_api = parse_version(torch.__version__) >= parse_version('1.11.0') # Allow prerelease builds of 1.11
 
 class AdaptiveAugment:
     def __init__(self, ada_aug_target, ada_aug_len, update_every, device):
@@ -357,10 +360,14 @@ class GridSampleForward(autograd.Function):
 class GridSampleBackward(autograd.Function):
     @staticmethod
     def forward(ctx, grad_output, input, grid):
-        op = torch._C._jit_get_operation("aten::grid_sampler_2d_backward")
-        grad_input, grad_grid = op(grad_output, input, grid, 0, 0, False)
+        op, _ = torch._C._jit_get_operation('aten::grid_sampler_2d_backward')
+        if _use_pytorch_1_11_api:
+            output_mask = (ctx.needs_input_grad[1], ctx.needs_input_grad[2])
+            # print(type(op), op)
+            grad_input, grad_grid = op(grad_output, input, grid, 0, 0, False, output_mask)
+        else:
+            grad_input, grad_grid = op(grad_output, input, grid, 0, 0, False)
         ctx.save_for_backward(grid)
-
         return grad_input, grad_grid
 
     @staticmethod
